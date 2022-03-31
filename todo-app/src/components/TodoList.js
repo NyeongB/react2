d8dc189f820a0998657b97cca98d85c53ac98ad3
NyeongB <kdtoaj14@ajou.ac.kr>
2022-03-31 19:51:24 +0900

    p.284

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -2,11 +2,15 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onRemove }) => {
   return (
     <div>
       {todos.map((todo) => (
-        <TodoListItem todo={todo}></TodoListItem>
+        <TodoListItem
+          todo={todo}
+          key={todo.id}
+          onRemove={onRemove}
+        ></TodoListItem>
       ))}
     </div>
   );
